e6f03ce6ab7f275cabc4f1d39d533eec13471dce
manuel <sergio.rudenko@gmail.com>
2016-03-18 17:11:52 +0300

    data synchronization via ws (subscriptions)

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,20 +9,6 @@ console.log("application started!");
  * Conditionally loads webcomponents polyfill if needed.
  * Credit: Glen Maddern (geelen on GitHub)
  */
-var webComponentsSupported = ('registerElement' in document
-&& 'import' in document.createElement('link')
-&& 'content' in document.createElement('template'));
-
-if (!webComponentsSupported) {
-    var wcPoly = document.createElement('script');
-    wcPoly.src = '/bower_components/webcomponentsjs/webcomponents-lite.min.js';
-    wcPoly.onload = lazyLoadPolymerAndElements;
-    document.head.appendChild(wcPoly);
-
-} else {
-    lazyLoadPolymerAndElements();
-}
-
 function lazyLoadPolymerAndElements() {
     // Let's use Shadow DOM if we have it, because awesome.
     window.Polymer = window.Polymer || {};
@@ -30,7 +16,6 @@ function lazyLoadPolymerAndElements() {
 
     var elements = [
         //'/path/to/bundle/one.html',
-        //'/elements/my-view.html'
         '/elements/config-app/layout.html',
         '/elements/config-app/sources-list.html',
         '/elements/config-app/variables-list.html',
@@ -47,4 +32,99 @@ function lazyLoadPolymerAndElements() {
     });
 }
 
-//TODO: ws dialog
+var webComponentsSupported = ('registerElement' in document
+&& 'import' in document.createElement('link')
+&& 'content' in document.createElement('template'));
+
+if (!webComponentsSupported) {
+    var wcPoly = document.createElement('script');
+    wcPoly.src = '/bower_components/webcomponentsjs/webcomponents-lite.min.js';
+    wcPoly.onload = lazyLoadPolymerAndElements;
+
+    document.head.appendChild(wcPoly);
+
+} else {
+    lazyLoadPolymerAndElements();
+
+}
+
+
+// WebSocket --------------------
+var wsuri = 'ws://localhost:4080';
+var ws = null;
+
+function wsConnect() {
+    if ("WebSocket" in window) {
+        ws = new WebSocket(wsuri);
+    } else if ("MozWebSocket" in window) {
+        ws = new MozWebSocket(wsuri);
+    } else {
+        console.log('Browser does not support WebSocket!');
+    }
+
+    if (ws) {
+        ws.onopen = function () {
+            console.log('ws: Connection established.');
+            ws.send('hello ws server!', function ack(error) {
+                if (error)
+                    console.log('ws: Send error: ' + error.message);
+            });
+        };
+
+        ws.onerror = function (error) {
+            console.log('ws: Error: ' + error.message);
+        };
+
+        ws.onclose = function (event) {
+            if (event.wasClean)
+                console.log('ws: Connection closed cleanly.');
+            else
+                console.log('ws: Disconnected, code: "%s", reason: "%s"', event.code, event.reason);
+        };
+
+        // processing data
+        ws.onmessage = function (event) {
+            switch (event.data) {
+                case 'AUTHENTICATE:': //TODO: secure authentication
+                    var authData = {
+                        func: 'AUTHENTICATION',
+                        user: 'manuel',
+                        password: 'qwerty'
+                    };
+
+                    try {
+                        authData = JSON.stringify(authData);
+                        ws.send(authData);
+                    } catch (error) {
+                        console.error(error);
+                    }
+                    break;
+
+                case 'AUTHENTICATION: PASSED':
+                    console.log('ws: Authentication passed.');
+                    break;
+
+                case 'AUTHENTICATION: FAILED':
+                    console.log('ws: Authentication failed!');
+                    break;
+
+                //TODO: channel operations
+
+                default:
+                    console.log('ws: Received data: ' + event.data);
+            }
+        };
+    }
+}
+
+function wsConnectionCheck() {
+    if (!ws || ws.readyState == 3) wsConnect();
+    //console.log(new Date());
+}
+
+wsConnect(); //init connection
+setInterval(wsConnectionCheck, 5000); //check every 5s
+
+
+
+
--- server.js
+++ server.js
@@ -16,26 +16,104 @@ var server = require('http').createServer(),
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(__dirname + '/app'));
 
-wss.on('connection', function connection(ws) {
-    var location = url.parse(ws.upgradeReq.url, true);
+// manage ws clients
+var wsClients = {};
+wss.on('connection', function (ws) {
+    //var location = url.parse(ws.upgradeReq.url, true);
     // you might use location.query.access_token to authenticate or share sessions
     // or ws.upgradeReq.headers.cookie (see http://stackoverflow.com/a/16395220/151312)
+    //console.log(ws);
 
-    ws.on('message', function incoming(message) {
-        console.log('ws received: %s', message);
+    var wskey = ws.upgradeReq.headers['sec-websocket-key'];
+    wsClients[wskey] = {
+        socket: ws,
+        authenticated: false,
+        subscriptions: []
+    };
+
+    ws.onclose = function (event) {
+        console.log('ws "%s" closed with code: %s', wskey, event.code);
+        delete wsClients[wskey];
+        //console.log(wsClients);
+    };
+
+    ws.onmessage = function (event) {
+        try {
+            var data = {};
+            data = JSON.parse(event.data);
+
+            switch (data.func) {
+                case 'AUTHENTICATION':
+                    _wsClientLogin(wskey, data);
+                    break;
+
+                case 'SUBSCRIPT':
+                    if (wsClients[wskey].subscription)
+                        _wsClientSubscript(wskey, data);
+                    else
+                        _wsClientLogout(wskey, 'NOT AUTHORISED');
+                    break;
+
+                case 'LOGOUT':
+                    _wsClientLogout(wskey, 'LOGGED OUT...');
+                    break;
+
+                //TODO: subscription
+
+                default:
+                    console.log(data);
+            }
+
+        } catch (error) {
+            console.log('ws "%s" receive: "%s"', wskey, event.data);
+            //console.error(error);
+        }
+    };
+
+    ws.send('AUTHENTICATE:', function ack(error) {
+        if (error) console.log('ws "%s" send error: %s', wskey, error.message);
     });
-
-    ws.send('greeting');
 });
 
+// server start
 server.on('request', app);
 server.listen(process.env.PORT || port || 3000, function () {
     console.log('Listening on ' + server.address().port);
 });
 
+function _wsClientSubscript(key, data) {
+    //{func: 'SUBSCRIPT', channel: 'variables', data: [{id: 10, per: 1000}, {id: 11}]}
+
+}
+
+function _wsClientLogin(key, data) {
+    if (data.user != 'manuel' || data.password != 'qwerty') { //TODO: secure authentication
+        wsClients[key].socket.send('AUTHENTICATION: FAILED', function (error) {
+            if (error) console.log('ws "%s" send error: %s', wskey, error.message);
+            wsClients[key].socket.terminate();
+        });
+
+    } else {
+        wsClients[key].socket.send('AUTHENTICATION: PASSED');
+        wsClients[key].authenticated = true;
+        wsClients[key].authdata = data;
+    }
+}
+
+function _wsClientLogout(key, message) {
+    wsClients[key].socket.send(message, function (error) {
+        if (error) console.log('ws "%s" send error: %s', key, error.message);
+        wsClients[key].socket.terminate();
+    });
+}
+
+
+
+
+
 
 // database -------------------------------------------------------------------
-var tables = {};
+var channels = {};
 
 var pg = require('pg');
 var pgConString = 'postgres://manuel:0560044@192.168.5.29/manuel';
@@ -57,12 +135,14 @@ function _parsePayload(msg) {
     //'78801100f4c2dfbb5e6afdb2dde95194:1:1:{"table":"variables","op":"INSERT",
     // "data":[{"id":2,"type":2,"data":{},"created_at":"2016-03-17T15:38:45.097962+03:00",
     // "modified_at":"2016-03-17T15:38:45.097962+03:00"}]}'
+    var pld = {};
+
     try {
-        return JSON.parse(msg.payload.split(':').slice(3).join(':'));
+        pld = JSON.parse(msg.payload.split(':').slice(3).join(':'));
 
     } catch (err) {
-        return {
-            table: 'none',
+        pld = {
+            table: 'messages',
             op: 'NOTIFY',
             data: {
                 processId: msg.processId,
@@ -70,28 +150,70 @@ function _parsePayload(msg) {
             }
         };
     }
+
+    _checkTableChannel(pld.table);
+    return pld;
+}
+
+function _checkTableChannel(name) {
+    if (channels[name] !== undefined) return;
+
+    // init channel
+    channels[name] = [];
+    channels[name].push({subscribers: []});
+
+    if (name === 'messages') return;
+
+    // get records from database
+    pgClient.query("SELECT * FROM " + name + " WHERE id > 0 ORDER BY id",
+        function (err, result) {
+            if (err) return console.error('error running query', err);
+
+            for (var i = 0; i < result.rows.length; i++)
+                channels[name][result.rows[i].id] = result.rows[i];
+
+            console.log('initialized channel: "%s", %s row(s)',
+                name, channels[name].length);
+        });
+}
+
+function _updateSubscribers(channel, data) {
+    try {
+        var list = channels[table][0].subscribers;
+
+        for (var i = 0; i < list.length; i++)
+            list[i].socket.send(JSON.stringify(data));
+
+    } catch (error) {
+        console.error(error);
+    }
+
 }
 
 function _insertRow(table, data) {
     //console.log('insert into "%s": "id:%s"', table, data[0].id);
-    if (tables[table] === undefined)
-        tables[table] = [];
-
-    tables[table][data[0].id] = data;
+    channels[table][data[0].id] = data;
+    _updateSubscribers(table, data);
 }
 
 function _deleteRow(table, data) {
     //console.log('delete from "%s": "id:%s"', table, data[0].id);
-    if (tables[table] === undefined)
-        tables[table] = [];
+    delete channels[table][data[0].id];
 
-    delete tables[table][data[0].id];
+    _updateSubscribers(table, {
+        table: table,
+        op: 'DELETE',
+        data: [{id: data[0].id}]
+    });
+}
+
+function _updateRow(table, data) {
+    //TODO: update data and subscribers
 }
 
 function _customNotify(data) {
     console.log('notify pid[%s]: "%s"', data.processId, data.text);
 }
-
 
 // notification listener
 pgClient.on('notification', function (msg) {
@@ -118,33 +240,17 @@ pgClient.on('notification', function (msg) {
             console.error('Can`t process notify:');
             console.error(pld);
     }
-
-    if (pld.table === 'variables')
-        console.log(tables[pld.table]);
+    //if (pld.table === 'variables')
+    //    console.log(channels[pld.table]);
 });
 
 // set to listen notifications on channel 'livepg'
 // by our trigger
 pgClient.query("LISTEN livepg");
 
-// simple select
-//pgClient.query("SELECT * FROM variables ORDER BY id", function(err, result) {
-//    if (err) {
-//        return console.error('error running query', err);
-//    }
-//    //console.log(result.rows);
-//});
-
-//// custom send notification
-//pgClient.query("NOTIFY livepg, 'test via NOTIFY'");
-//
-//// custom send notification, other way
-//pgClient.query("SELECT pg_notify('livepg', 'test via SELECT')", function(err, result) {
-//    if (err) {
-//        return console.error('error running query', err);
-//    }
-//});
-
+// custom send notification (and init channel "messages")
+// (other way: "SELECT pg_notify('livepg', 'test via SELECT')")
+pgClient.query("NOTIFY livepg, 'web server say: hello!'");
 
 // close connection and cleanup on server exit
 process.on('SIGINT', function () {
